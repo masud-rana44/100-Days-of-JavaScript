33fdf077a7c72cbdd36b1f1d36c9e74e196de370
masud-rana44 <mdmasudrana4488@gmail.com>
2023-04-03 00:45:37 +0600

    Add pixel art generator

--- Day #1 - Pixel Art Generator/Final/index.js
+++ Day #1 - Pixel Art Generator/Final/index.js
@@ -9,114 +9,59 @@ let paintBtn = document.getElementById("paint-btn");
 let widthValue = document.getElementById("width-value");
 let heightValue = document.getElementById("height-value");
 
-let events = {
-    mouse: {
-        down: "mousedown",
-        move: "mousemove",
-        up: "mouseup"
-    },
-    touch: {
-        down: "touchstart",
-        mobe: "touchmove",
-        up: "touchend",
-    },
+let isPaint = true;
+
+const clearGridMarkup = function () {
+  container.innerHTML = "";
 };
 
-let deviceType = "";
+const renderGridMarkup = function () {
+  let markup = "";
+  const row = +heightValue.textContent;
+  const col = +widthValue.textContent;
 
-let draw = false;
-let erase = false;
-
-const isTouchDevice = () => {
-    try {
-        document.createEvent("TouchEvent");
-        deviceType = "touch";
-        return true;
-    } catch (e) {
-        deviceType = "mouse";
-        return false;
+  for (let i = 0; i < row; i++) {
+    markup += '<div class="gridRow">';
+    for (let j = 0; j < col; j++) {
+      markup += '<div class="gridCol"></div>';
     }
+    markup += "</div>";
+  }
+
+  clearGridMarkup();
+  container.insertAdjacentHTML("beforeend", markup);
 };
 
-isTouchDevice();
+// Events
+const events = [gridWidth, gridHeight];
 
-gridButton.addEventListener("click", () => {
-    container.innerHTML = "";
-    let count = 0;
-    for (let i = 0; i < gridHeight.value; i++) {
-        count += 2;
-        let div = document.createElement("div");
-        div.classList.add("gridRow");
+window.onload = () => {
+  gridHeight.value = 0;
+  gridWidth.value = 0;
+};
 
-        for (let j = 0; j < gridWidth.value; j++) {
-            count += 2;
-            let col = document.createElement("div");
-            col.classList.add("gridCol");
-            col.setAttribute("id", `gridCol${count}`);
-            col.addEventListener(events[deviceType].down, () => {
-                draw = true;
-                if (erase) {
-                    col.style.backgroundColor = "transparent";
-                } else {
-                    col.style.backgroundColor = colorButton.value;
-                }
-            });
-
-            col.addEventListener(events[deviceType].move, (e) => {
-                let elementId = document.elementFromPoint(
-                    !isTouchDevice() ? e.clientX : e.touches[0].clientX,
-                    !isTouchDevice() ? e.clientY : e.touches[0].clientY,
-                ).id;
-                checker(elementId);
-            });
-
-            col.addEventListener(events[deviceType].up, () => {
-                draw = false;
-            });
-
-            div.appendChild(col);
-
-        }
-
-        container.appendChild(div);
-
-    }
-});
-
-function checker(elementId) {
-    let gridColumns = document.querySelectorAll(".gridCol");
-    gridColumns.forEach((element) => {
-        if (elementId == element.id) {
-            if (draw && !erase) {
-                element.style.backgroundColor = colorButton.value;
-            } else if (draw && erase) {
-                element.style.backgroundColor = "transparent";
-            }
-        }
-    });
-}
-
-clearGridButton.addEventListener("click", () => {
-    container.innerHTML = "";
-});
+events.forEach((event) =>
+  event.addEventListener("input", () => {
+    if (event === gridWidth)
+      widthValue.textContent = `${event.value}`.padStart(2, 0);
+    else heightValue.textContent = `${event.value}`.padStart(2, 0);
+  })
+);
 
 eraseBtn.addEventListener("click", () => {
-    erase = true;
+  isPaint = false;
 });
 
 paintBtn.addEventListener("click", () => {
-    erase = false;
+  isPaint = true;
 });
 
-gridWidth.addEventListener("input", () => {
-    widthValue.innerHTML = gridWidth.value < 9 ? `0${gridWidth.value}` : gridWidth.value;
-});
+clearGridButton.addEventListener("click", clearGridMarkup);
+gridButton.addEventListener("click", renderGridMarkup);
 
-gridHeight.addEventListener("input", () => {
-    heightValue.innerHTML = gridHeight.value < 9 ? `0${gridHeight.value}` : gridHeight.value;
-});
+container.addEventListener("mousemove", (e) => {
+  const gridCell = e.target.closest(".gridCol");
+  if (!gridCell) return;
 
-window.onload = () => {
-    gridHeight.value = 0;
-    gridWidth.value = 0;
-};
+  gridCell.style.backgroundColor = `${isPaint ? colorButton.value : "white"}`;
+});
