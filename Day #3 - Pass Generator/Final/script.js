91c09797628f87d3ef4c93a941cec6b21ce6b272
masud-rana44 <mdmasudrana4488@gmail.com>
2023-04-18 23:08:28 +0600

    Complete pass generator

--- Day #3 - Pass Generator/Final/script.js
+++ Day #3 - Pass Generator/Final/script.js
@@ -2,12 +2,21 @@ const lengthSlider = document.querySelector(".pass-length input");
 const options = document.querySelectorAll(".option input");
 const copyIcon = document.querySelector(".input-box span");
 const passwordInput = document.querySelector(".input-box input");
+const passLengthValue = document.querySelector(".pass-length span");
 const passIndicator = document.querySelector(".pass-indicator");
 const generateBtn = document.querySelector(".generate-btn");
 
-function generateRandomString(length) {
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+:;/<>{}[]";
+const spaces = "      ";
+
+const settings = [lowercase];
+// "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+function generateRandomPassword(length) {
+  const characters = settings.join("");
   let result = "";
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
@@ -15,13 +24,57 @@ function generateRandomString(length) {
   return result;
 }
 
-lengthSlider.addEventListener("change", () => {
+lengthSlider.addEventListener("input", () => {
+  // Read the value & Show it
   const value = lengthSlider.value * 1;
-  if (value <= 30) passIndicator.setAttribute("id", "strong");
-  if (value <= 15) passIndicator.setAttribute("id", "medium");
-  if (value <= 8) passIndicator.setAttribute("id", "weak");
+  passLengthValue.innerHTML = value;
+
+  // Update the random string
+  const randomPassword = generateRandomPassword(value);
+  passwordInput.value = randomPassword;
+
+  // Update the indicator
+  if (value <= 30) passIndicator.id = "strong";
+  if (value <= 15) passIndicator.id = "medium";
+  if (value <= 8) passIndicator.id = "weak";
 });
 
 options.forEach((op) => {
-  if (op.checked) console.log(op.id);
+  op.addEventListener("change", () => {
+    if (op.id === "uppercase") {
+      if (op.checked && !settings.includes(uppercase)) settings.push(uppercase);
+      else
+        settings.splice(
+          settings.findIndex((el) => el === uppercase),
+          1
+        );
+    } else if (op.id === "numbers") {
+      if (op.checked && !settings.includes(numbers)) settings.push(numbers);
+      else
+        settings.splice(
+          settings.findIndex((el) => el === numbers),
+          1
+        );
+    } else if (op.id === "symbols") {
+      if (op.checked && !settings.includes(symbols)) settings.push(symbols);
+      else
+        settings.splice(
+          settings.findIndex((el) => el === symbols),
+          1
+        );
+    } else if (op.id === "spaces") {
+      if (op.checked && !settings.includes(spaces)) settings.push(spaces);
+      else
+        settings.splice(
+          settings.findIndex((el) => el === spaces),
+          1
+        );
+    }
+  });
+});
+
+generateBtn.addEventListener("click", () => {
+  const value = lengthSlider.value * 1;
+  const randomPassword = generateRandomPassword(value);
+  passwordInput.value = randomPassword;
 });
